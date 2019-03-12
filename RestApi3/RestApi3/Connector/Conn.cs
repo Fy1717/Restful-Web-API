8e022b1486925469592850b04a7175a32a82aed1
Furkan Yıldız <frknyldz8489@gmail.com>
2019-03-12 06:24:27 +0300

    First Commit For API

---
+++ RestApi3/RestApi3/Connector/Conn.cs
@@ -0,0 +1,34 @@
+﻿using RestApi3.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestApi3.Connector
+{
+    public class Conn
+    {
+        private string connstr = "Server=DESKTOP-HQ2891Q;Database=CafeMenu;Trusted_Connection=True";
+
+        public List<Kategoriler> kategoriListele()
+        {
+            List<Kategoriler> kategoriler = new List<Kategoriler>();
+            using (SqlConnection conn = new SqlConnection(connstr))
+            {
+                string komut = "select * from dbo.Kategoriler ";
+                SqlCommand com = new SqlCommand(komut, conn);
+                conn.Open();
+                SqlDataReader dr = com.ExecuteReader();
+                while (dr.Read())
+                {
+                    kategoriler.Add(new Kategoriler { ID = dr.GetInt32(dr.GetOrdinal("ID")), ADI = dr.GetString(dr.GetOrdinal("ADI")) });
+                }
+                conn.Close();
+            }
+            return kategoriler;
+
+        }
+
+    }
+}
---
+++ RestApi3/RestApi3/Model/Kategoriler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestApi3.Model
+{
+    public class Kategoriler
+    {
+        public int ID { get; set; }
+        public string ADI { get; set; }
+    }
+}
